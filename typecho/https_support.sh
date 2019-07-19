af61231960f2a4f8abf4c8c8953725ca2b77e5e6
oneisall8955 <oneisall8955@gmail.com>
2019-07-20 00:20:35 +0800

    https & http2 module

--- typecho/https_support.sh
+++ typecho/https_support.sh
@@ -16,29 +16,29 @@ source ${automatic_dir}/basic_shell.sh
 etc_profile="/etc/profile"
 NGINX_CONF_DIR=${NGINX_CONF_DIR}
 if [[ ! -f "${etc_profile}" ]] && [[ "${NGINX_CONF_DIR}X" == "X" ]];then
-    error "找不到:环境变量配置文件:${etc_profile},失败"
+    error $LINENO "找不到:环境变量配置文件:${etc_profile},失败"
     exit 1
 fi
 source "/etc/profile"
 nginx_conf_dir=${NGINX_CONF_DIR}
 if [ ! -d ${nginx_conf_dir} ] ;then
-    error "nginx配置文件路径不存在:${nginx_conf_dir}"
+    error $LINENO "nginx配置文件路径不存在:${nginx_conf_dir}"
     exit 1
 fi
-info_var "nginx_conf_dir"
+info_var $LINENO "nginx_conf_dir"
 
 TYPECHO_HOME=${TYPECHO_HOME}
-if [ ! -d ${TYPECHO_HOME} ] ;then
-    error "typecho路径不存在:${TYPECHO_HOME}"
+info_var $LINENO "TYPECHO_HOME"
+if [[ "${TYPECHO_HOME}X" == "X" ]] || [[ ! -d ${TYPECHO_HOME} ]] ;then
+    error $LINENO "typecho路径不存在:${TYPECHO_HOME}"
     exit 1
 fi
-info_var "nginx_conf_dir"
 
 domain=$1
-info "domain" "输入的域名"
+info_var $LINENO "domain" "输入的域名"
 if [[ "${domain}X" == "X" ]]; then
-    error "请传递域名!!!"
-    error "Usage: bash https_support.sh 'hello.example.com'"
+    error $LINENO "请传递域名!!!"
+    error $LINENO "Usage: bash https_support.sh 'hello.example.com'"
     exit 1
 fi
 cd ${nginx_conf_dir}/..
@@ -47,31 +47,31 @@ nginx_ssl_dir="${nginx_home}/ssl"
 info_var 'nginx_home'
 info_var 'nginx_ssl_dir'
 if [[ ! -d "${nginx_ssl_dir}" ]]; then
-    error "nginx默认ssl证书配置文件夹不存在"
+    error $LINENO "nginx默认ssl证书配置文件夹不存在"
 fi
 cd ${execute_path}
 certificate_file="${nginx_ssl_dir}/${domain}.zip"
-if [[ ! f ${certificate_file} ]];then
-    error "域名${domain}的证书压缩包${certificate_file}不存在"
-    error "请在${nginx_ssl_dir}下放置${domain}.zip"
+if [[ ! -f ${certificate_file} ]];then
+    error $LINENO "域名${domain}的证书压缩包${certificate_file}不存在"
+    error $LINENO "请在${nginx_ssl_dir}下放置${domain}.zip"
     exit 1
 fi
 
 #check if nginx support https ?
 nginx -V
-NGINX_HTTPS_SUPPORT=${NGINX_HTTPS_SUPPORT}
-info_var 'NGINX_HTTPS_SUPPORT'
-if [[ ${NGINX_HTTPS_SUPPORT} !=1  ]]; then
-    warn "可能当前nginx没有安装https模块!"
+NGINX_HTTPS_SUPPORT=${NGINX_HTTPS_SUPPORT:-0}
+info_var $LINENO "NGINX_HTTPS_SUPPORT"
+if [[ ${NGINX_HTTPS_SUPPORT} != 1 ]]; then
+    warn "当前nginx可能没有安装https模块!"
     warn "如已经安装Https支持,请在${nginx_conf_dir}目录下添加profile文件设置NGINX_HTTPS_SUPPORT=1"
-    if read -t 10 -p "yes/YES/Y/y选择忽略警告,继续安装?10s后不输入默认不安装)" continue_install
+    if read -t 10 -p "yes/YES/Y/y选择忽略警告,继续安装?10s后不输入默认不安装" continue_install
     then
         continue_install=`echo ${continue_install} | tr '[a-z]' '[A-Z]'`
-        if [[ ${continue_install} == "YES" || ${continue_install} =="Y" ]];then
-            info_var "continue_install"
+        if [[ ${continue_install} == "YES" || ${continue_install} == "Y" ]];then
+            info_var $LINENO "continue_install"
             warn "选择忽略警告,继续安装!!!"
         else
-            info_var "continue_install"
+            info_var $LINENO "continue_install"
             warn "选择停止安装,默认退出"
             exit 0
         fi
@@ -91,24 +91,25 @@ fi
 #---- Apache/
 #------ ......
 #------ ......
-certificate_unzip_dir="${nginx_ssl_dir}/${domain}"}
+certificate_unzip_dir="${nginx_ssl_dir}/${domain}"
+info_var $LINENO "certificate_unzip_dir"
 mkdir -p ${certificate_unzip_dir}
-tar -zxvf ${certificate_file} -C "${certificate_unzip_dir}"
+unzip -o -d ${certificate_unzip_dir} ${certificate_file}
 blog_conf="${nginx_conf_dir}/blog.conf"
-info_var "blog_conf" "原博客配置文件"
+info_var $LINENO "blog_conf" "原博客配置文件"
 if [[ -f "${blog_conf}" ]]; then
     nowTime=`date +'%Y%m%d_%H%M%S'`
-    info_var "blog_conf"
+    info_var $LINENO "blog_conf"
     mv ${blog_conf} "${blog_conf}.bak_${nowTime}"
 fi
 ssl_crt_file="${certificate_unzip_dir}/Nginx/1_${domain}_bundle.crt"
 ssl_key_file="${certificate_unzip_dir}/Nginx/2_${domain}.key"
-info_var "ssl_crt_file" "ssl证书:crt文件"
-info_var "ssl_key_file" "ssl证书:key文件"
+info_var $LINENO "ssl_crt_file" "ssl证书:crt文件"
+info_var $LINENO "ssl_key_file" "ssl证书:key文件"
 if [[ ! -f ${ssl_crt_file} ]] || [[ ! -f ${ssl_key_file} ]]; then
-    error "解压包异常!crt文件/key文件不存在"
+    error $LINENO "解压包异常!crt文件/key文件不存在"
     set +e
-    info "解压包文件列表如下"
+    info $LINENO "解压包文件列表如下"
     ls -l ${certificate_unzip_dir}
     set -e
     exit 1
@@ -116,8 +117,11 @@ fi
 blog_ssl_conf="${nginx_conf_dir}/blog_ssl.conf"
 php_fpm_service=`ls -lt /lib/systemd/system/ |grep 'fpm.service' | head -n 1 |awk '{print $NF}'`
 php_sock=`echo ${php_fpm_service} | sed 's/.service/.sock/g'`
+info_var $LINENO "blog_ssl_conf"
+info_var $LINENO "php_fpm_service"
+info_var $LINENO "php_sock"
 if [[ -f "${php_sock}"  ]]; then
-    error "sock 文件不存在"
+    error $LINENO "sock 文件不存在"
     exit 1
 fi
 
@@ -157,12 +161,12 @@ server {
     }
 
     if (!-e \$request_filename) {
-            rewrite ^(.*)$ /index.php$1 last;
+            rewrite ^(.*)$ /index.php\$1 last;
     }
 
     location ~ .*\.php(\/.*)*$ {
             include fastcgi.conf;
-            fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+            fastcgi_pass unix:/run/php/${php_sock};
             include fastcgi_params;
     }
 
