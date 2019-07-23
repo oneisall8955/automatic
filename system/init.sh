e5ffb969bb5e3d833a5d8dd97ce0c2e0d2165fc7
oneisall8955 <oneisall8955@gmail.com>
2019-07-23 17:54:32 +0800

    system init optimize

--- system/init.sh
+++ system/init.sh
@@ -12,6 +12,8 @@ cd ${execute_path}
 #inclue basic function
 source ${automatic_dir}/basic_shell.sh
 
+info "系统初始化ssh/git 开始..."
+
 while read line
 do
     warn $LINENO ${line}
@@ -29,9 +31,11 @@ Port 22122
 EOF
 `
 sleep 5s
-
+echo ""
+echo ""
 echo ""
 
+info "公私钥生成程序start..."
 if read -t 10 -p "yes/YES/Y/y 是否生成公钥私钥?" keygen
 then
     keygen=`echo ${keygen} | tr '[a-z]' '[A-Z]'`
@@ -42,10 +46,19 @@ then
 else
     warn "跳过生成公钥部分!"
 fi
+info "公私钥生成程序end..."
+echo ""
+echo ""
+echo ""
 
 info "git安装检查..."
 apt-get -y install git
 info "检查完毕"
+echo ""
+echo ""
+echo ""
+
+info "git 全局配置start..."
 if read -t 10 -p "填写git用户名称!" name
 then
     info_var $LINENO "name" "user.name"
@@ -63,10 +76,18 @@ then
         git config --global user.email "${email}"
     fi
 else
-    warn "跳过设置:git config --global user.name 'your name' "
+    warn "跳过设置:git config --global user.email 'your email' "
 fi
+info "git 全局配置end..."
+echo ""
+echo ""
+echo ""
 
+info "请检测git配置..."
 git config -l
 sleep 5s
 
+echo ""
+echo ""
+echo ""
 info "ssh && git 初始化完毕"
