794a2cb4d44c53d4e35527d3d368800f2fceadc0
oneisall8955 <oneisall8955@gmail.com>
2019-07-24 13:36:11 +0800

    copy code fix

--- zsh/install.sh
+++ zsh/install.sh
@@ -150,8 +150,8 @@ EOF
 `
 #if [ -d /etc/profile.d ]; then
 #  for i in /etc/profile.d/*.sh; do
-#    if [ -r \$i ]; then
-#      . \$i
+#    if [ -r $i ]; then
+#      . $i
 #    fi
 #  done
 #  unset i
