23cdcaeb780c6ddd84caf1bd34508510432873fc
oneisall8955 <oneisall8955@gmail.com>
2019-11-13 14:31:49 +0800

    fix ssh

--- running/secure_ssh.sh
+++ running/secure_ssh.sh
@@ -5,7 +5,7 @@
 SSH_LOG_FILE=/var/log/auth.log
 
 WORK_DIR="/opt/temp"
-mkdir ${WORK_DIR}
+mkdir -p ${WORK_DIR}
 BLACK_LIST_FILE=${WORK_DIR}/black.txt
 
 # black ip count status
@@ -14,18 +14,20 @@ cat ${SSH_LOG_FILE}|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"="
 # limit failed time
 DEFINE=5
 
-for i in `cat ${BLACK_LIST_FILE}`
+for element in `cat ${BLACK_LIST_FILE}`
 do
-	IP=`echo $i |awk -F= '{print $1}'`
-	NUM=`echo $i|awk -F= '{print $2}'`
+    IP=`echo ${element} |awk -F= '{print $1}'`
+    NUM=`echo ${element}|awk -F= '{print $2}'`
 
-	# greater than limit
-	if [ ${NUM} -gt ${DEFINE} ]; then
-		# check if exist ? exist , return 0 ; not exist , return 1
-		grep ${IP} /etc/hosts.deny > /dev/null
-	fi
-	# $? > 0 , it means : $NUM > $DEFINE and IP doesn't exist in /etc/hosts.deny
-	if [ $? -gt 0 ]; then
-		echo "sshd:${IP}" >> /etc/hosts.deny
-	fi
+    # greater than limit
+    if [ ${NUM} -gt ${DEFINE} ]; then
+        # check if exist ? exist , return 0 ; not exist , return 1
+        grep ${IP} /etc/hosts.deny > /dev/null
+    fi
+    # $? > 0 , it means : $NUM > $DEFINE and IP doesn't exist in /etc/hosts.deny
+    if [ $? -gt 0 ]; then
+        echo "sshd:${IP}" >> /etc/hosts.deny
+    fi
 done
+sort -t= -k2 -n ${BLACK_LIST_FILE} > ${BLACK_LIST_FILE}_
+mv ${BLACK_LIST_FILE}_ ${BLACK_LIST_FILE}
